0a7601b8c0fa9b3c0ecb673276abc09a08f134d5
jviccora <joe.viccora@libertasfunding.com>
2020-04-20 12:01:02 -0400

    fixing history

--- index.js
+++ index.js
@@ -3,79 +3,78 @@ const inquirer = require("inquirer");
 const axios = require("axios");
 
 const questions = [
-  "What is your GitHub user name?",
-  "What is the name of the project (or README file)?",
-  "Give a brief description of the project",
-  "What type of licesce should the project have?",
-  "What is the command to install dependencies?",
-  "What is the command to run tests?",
-  "What knowledge is required to use this repository?",
-  "What knowledge is required to contribute to this repository?"
+    "What is your GitHub user name?",
+    "What is the name of the project (or README file)?",
+    "Give a brief description of the project",
+    "What type of licesce should the project have?",
+    "What is the command to install dependencies?",
+    "What is the command to run tests?",
+    "What knowledge is required to use this repository?",
+    "What knowledge is required to contribute to this repository?"
 ];
 
 let init = () => {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "username",
-        message: questions[0]
-      },
-      {
-        type: "input",
-        name: "project",
-        message: questions[1]
-      },
-      {
-        type: "input",
-        name: "description",
-        message: questions[2]
-      },
-      {
-        type: "list",
-        message: questions[3],
-        name: "license",
-        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
-      },
-      {
-        type: "input",
-        name: "installation",
-        message: questions[4]
-      },
-      {
-        type: "input",
-        name: "tests",
-        message: questions[5]
-      },
-      {
-        type: "input",
-        name: "usage",
-        message: questions[6]
-      },
-      {
-        type: "input",
-        name: "contributing",
-        message: questions[7]
-      }
-    ])
-    .then(function(response) {
-      const queryUrl = `https://api.github.com/users/${response.username}`;
+    inquirer
+        .prompt([{
+                type: "input",
+                name: "username",
+                message: questions[0]
+            },
+            {
+                type: "input",
+                name: "project",
+                message: questions[1]
+            },
+            {
+                type: "input",
+                name: "description",
+                message: questions[2]
+            },
+            {
+                type: "list",
+                message: questions[3],
+                name: "license",
+                choices: ["APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+            },
+            {
+                type: "input",
+                name: "installation",
+                message: questions[4]
+            },
+            {
+                type: "input",
+                name: "tests",
+                message: questions[5]
+            },
+            {
+                type: "input",
+                name: "usage",
+                message: questions[6]
+            },
+            {
+                type: "input",
+                name: "contributing",
+                message: questions[7]
+            }
+        ])
+        .then(function(response) {
+            const queryUrl = `https://api.github.com/users/${response.username}`;
 
-      axios
-        .get(queryUrl)
-        .then(function(res) {
-          writeFile({ ...response, ...res.data });
+            axios
+                .get(queryUrl)
+                .then(function(res) {
+                    writeFile({...response, ...res.data });
+                })
+                .catch(err => console.log(err));
+
+            //   const repoNamesStr = repoNames.join("\n");
         })
         .catch(err => console.log(err));
-
-      //   const repoNamesStr = repoNames.join("\n");
-    })
-    .catch(err => console.log(err));
 };
 
 let writeFile = data => {
-  console.log(data);
-  let repoNamesStr = `
+    console.log(data);
+    let repoNamesStr = `
 # ${data.project}
 [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](${data.html_url})
     
@@ -127,11 +126,11 @@ ${data.tests}
 
 If you have any questions about the repo, open an issue or contact [${data.login}](${data.html_url}) directly at null.
     `;
-  fs.writeFile(`${data.project}.md`, repoNamesStr, function(err) {
-    if (err) {
-      throw err;
-    }
-  });
+    fs.writeFile(`${data.project}.md`, repoNamesStr, function(err) {
+        if (err) {
+            throw err;
+        }
+    });
 };
 
 init();
